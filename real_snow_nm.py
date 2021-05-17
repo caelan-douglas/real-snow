6bae155590132ccd6e61446daddd9392a50758fb
Caelan <caelan.douglas@outlook.com>
2021-05-17 00:48:28 -0400

    Remove material creation step

--- real_snow_nm.py
+++ real_snow_nm.py
@@ -1,6 +1,6 @@
 # Addon Info
 bl_info = {
-    "name": "Real Snow",
+    "name": "Real Snow (No material fork)",
     "description": "Generate snow mesh",
     "author": "Wolf <wolf.art3d@gmail.com>",
     "version": (1, 1),
@@ -32,7 +32,7 @@ class REAL_PT_snow(Panel):
     bl_context = "objectmode"
     bl_region_type = "UI"
     bl_label = "Snow"
-    bl_category = "Real Snow"
+    bl_category = "Real Snow (NMFork)"
 
     def draw(self, context):
         scn = context.scene
@@ -112,7 +112,6 @@ class SNOW_OT_Create(Operator):
                 coll = bpy.data.collections["Snow"]
             coll.objects.link(snow)
             context.view_layer.layer_collection.collection.objects.unlink(snow)
-            add_material(snow)
             # parent with object
             snow.parent = obj
             snow.matrix_parent_inverse = obj.matrix_world.inverted()
@@ -215,133 +214,6 @@ def area(obj: bpy.types.Object) -> float:
     area = sum(face.calc_area() for face in bm_obj.faces)
     bm_obj.free
     return area
-
-
-def add_material(obj: bpy.types.Object):
-    mat_name = "Snow"
-    # if material doesn't exist, create it
-    if mat_name in bpy.data.materials:
-	    bpy.data.materials[mat_name].name = mat_name+".001"
-    mat = bpy.data.materials.new(mat_name)
-    mat.use_nodes = True
-    nodes = mat.node_tree.nodes
-    # delete all nodes
-    for node in nodes:
-	    nodes.remove(node)
-    # add nodes
-    output = nodes.new('ShaderNodeOutputMaterial')
-    principled = nodes.new('ShaderNodeBsdfPrincipled')
-    vec_math = nodes.new('ShaderNodeVectorMath')
-    com_xyz = nodes.new('ShaderNodeCombineXYZ')
-    dis = nodes.new('ShaderNodeDisplacement')
-    mul1 = nodes.new('ShaderNodeMath')
-    add1 = nodes.new('ShaderNodeMath')
-    add2 = nodes.new('ShaderNodeMath')
-    mul2 = nodes.new('ShaderNodeMath')
-    mul3 = nodes.new('ShaderNodeMath')
-    ramp1 = nodes.new('ShaderNodeValToRGB')
-    ramp2 = nodes.new('ShaderNodeValToRGB')
-    ramp3 = nodes.new('ShaderNodeValToRGB')
-    vor = nodes.new('ShaderNodeTexVoronoi')
-    noise1 = nodes.new('ShaderNodeTexNoise')
-    noise2 = nodes.new('ShaderNodeTexNoise')
-    noise3 = nodes.new('ShaderNodeTexNoise')
-    mapping = nodes.new('ShaderNodeMapping')
-    coord = nodes.new('ShaderNodeTexCoord')
-    # change location
-    output.location = (100, 0)
-    principled.location = (-200, 500)
-    vec_math.location = (-400, 400)
-    com_xyz.location = (-600, 400)
-    dis.location = (-200, -100)
-    mul1.location = (-400, -100)
-    add1.location = (-600, -100)
-    add2.location = (-800, -100)
-    mul2.location = (-1000, -100)
-    mul3.location = (-1000, -300)
-    ramp1.location = (-500, 150)
-    ramp2.location = (-1300, -300)
-    ramp3.location = (-1000, -500)
-    vor.location = (-1500, 200)
-    noise1.location = (-1500, 0)
-    noise2.location = (-1500, -200)
-    noise3.location = (-1500, -400)
-    mapping.location = (-1700, 0)
-    coord.location = (-1900, 0)
-    # change node parameters
-    principled.distribution = "MULTI_GGX"
-    principled.subsurface_method = "RANDOM_WALK"
-    principled.inputs[0].default_value[0] = 0.904
-    principled.inputs[0].default_value[1] = 0.904
-    principled.inputs[0].default_value[2] = 0.904
-    principled.inputs[1].default_value = 1
-    principled.inputs[2].default_value[0] = 0.36
-    principled.inputs[2].default_value[1] = 0.46
-    principled.inputs[2].default_value[2] = 0.6
-    principled.inputs[3].default_value[0] = 0.904
-    principled.inputs[3].default_value[1] = 0.904
-    principled.inputs[3].default_value[2] = 0.904
-    principled.inputs[5].default_value = 0.224
-    principled.inputs[7].default_value = 0.1
-    principled.inputs[13].default_value = 0.1
-    vec_math.operation = "MULTIPLY"
-    vec_math.inputs[1].default_value[0] = 0.5
-    vec_math.inputs[1].default_value[1] = 0.5
-    vec_math.inputs[1].default_value[2] = 0.5
-    com_xyz.inputs[0].default_value = 0.36
-    com_xyz.inputs[1].default_value = 0.46
-    com_xyz.inputs[2].default_value = 0.6
-    dis.inputs[1].default_value = 0.1
-    dis.inputs[2].default_value = 0.3
-    mul1.operation = "MULTIPLY"
-    mul1.inputs[1].default_value = 0.1
-    mul2.operation = "MULTIPLY"
-    mul2.inputs[1].default_value = 0.6
-    mul3.operation = "MULTIPLY"
-    mul3.inputs[1].default_value = 0.4
-    ramp1.color_ramp.elements[0].position = 0.525
-    ramp1.color_ramp.elements[1].position = 0.58
-    ramp2.color_ramp.elements[0].position = 0.069
-    ramp2.color_ramp.elements[1].position = 0.757
-    ramp3.color_ramp.elements[0].position = 0.069
-    ramp3.color_ramp.elements[1].position = 0.757
-    vor.feature = "N_SPHERE_RADIUS"
-    vor.inputs[2].default_value = 30
-    noise1.inputs[2].default_value = 12
-    noise2.inputs[2].default_value = 2
-    noise2.inputs[3].default_value = 4
-    noise3.inputs[2].default_value = 1
-    noise3.inputs[3].default_value = 4
-    mapping.inputs[3].default_value[0] = 12
-    mapping.inputs[3].default_value[1] = 12
-    mapping.inputs[3].default_value[2] = 12
-    # link nodes
-    link = mat.node_tree.links
-    link.new(principled.outputs[0], output.inputs[0])
-    link.new(vec_math.outputs[0], principled.inputs[2])
-    link.new(com_xyz.outputs[0], vec_math.inputs[0])
-    link.new(dis.outputs[0], output.inputs[2])
-    link.new(mul1.outputs[0], dis.inputs[0])
-    link.new(add1.outputs[0], mul1.inputs[0])
-    link.new(add2.outputs[0], add1.inputs[0])
-    link.new(mul2.outputs[0], add2.inputs[0])
-    link.new(mul3.outputs[0], add2.inputs[1])
-    link.new(ramp1.outputs[0], principled.inputs[12])
-    link.new(ramp2.outputs[0], mul3.inputs[0])
-    link.new(ramp3.outputs[0], add1.inputs[1])
-    link.new(vor.outputs[4], ramp1.inputs[0])
-    link.new(noise1.outputs[0], mul2.inputs[0])
-    link.new(noise2.outputs[0], ramp2.inputs[0])
-    link.new(noise3.outputs[0], ramp3.inputs[0])
-    link.new(mapping.outputs[0], vor.inputs[0])
-    link.new(mapping.outputs[0], noise1.inputs[0])
-    link.new(mapping.outputs[0], noise2.inputs[0])
-    link.new(mapping.outputs[0], noise3.inputs[0])
-    link.new(coord.outputs[3], mapping.inputs[0])
-    # set displacement and add material
-    mat.cycles.displacement_method = "DISPLACEMENT"
-    obj.data.materials.append(mat)
-
 
 # Properties
 class SnowSettings(PropertyGroup):
